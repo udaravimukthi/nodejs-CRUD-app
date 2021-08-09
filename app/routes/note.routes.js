3be3b9f7577f7c93fc53dd2fd95100754962f0fa
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-10 04:54:20 +0530

    Added note.js

---
+++ app/routes/note.routes.js
@@ -0,0 +1,18 @@
+module.exports = (app) => {
+    const notes = require('../controllers/note.controller.js');
+
+    //create a new note
+    app.post('/notes',notes.create);
+
+    //retreive all the notes
+    app.get('/notes', notes.findAll);
+
+    //reteive a single note with a given id
+    app.get('/notes/:noteid', notes.findOne);
+
+    //update a note with a note id
+    app.get('/notes/:noteId', notes.update);
+
+    //delete a note with the given noteid
+    app.delete('/notes/:noteId', notes.delete);
+}
